741cb794e033631f547da74cbf195a73fc61ac56
ikramiste <ikrame.saadi@gmail.com>
2024-01-29 19:44:35 +0100

    skeleton

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Bienvenue dans Très Hypée Progame</h1>
+      {/* Ici, tu ajouteras le reste de ta logique d'application */}
+    </div>
+  );
+}
+
+export default App;
