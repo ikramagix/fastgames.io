534e34f5efefb4aae4173da2cbbb8b3fe8317772
ikramiste <ikrame.saadi@gmail.com>
2024-01-30 09:34:05 +0100

    renamed file

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Bienvenue sur <em>fastgames.io</em></h1>
+      {/* Ici, tu ajouteras le reste de ta logique d'application */}
+    </div>
+  );
+}
+
+export default App;
