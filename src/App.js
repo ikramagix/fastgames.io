a025a6e564e63ef78e105e5ffc63bc4efbcb2b07
ikramiste <ikrame.saadi@gmail.com>
2024-01-30 09:52:04 +0100

    webpk confg

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Bienvenue dans Très Hypée Progame</h1>
+      <h1>Bienvenue sur <em>fastgames.io</em></h1>
       {/* Ici, tu ajouteras le reste de ta logique d'application */}
     </div>
   );
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,63 @@
-const path = require("path");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  // Mode de développement pour un meilleur débogage
+  mode: 'development',
+  
+  // Point d'entrée de votre application
+  entry: './src/index.js',
+  
+  // Sortie des bundles
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
+  
+  // Règles pour les modules
   module: {
     rules: [
       {
+        // Transpile les fichiers .js avec Babel
         test: /\.js$/,
-        use: "babel-loader",
         exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        // Traitement des fichiers SCSS
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader, // extrait le CSS dans des fichiers séparés
+          'css-loader',   // traduit le CSS en CommonJS
+          'sass-loader'   // compile Sass en CSS
+        ],
       },
     ],
   },
+  
+  // Plugins
+  plugins: [
+    // Plugin pour générer l'index.html avec injection du bundle
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    
+    // Plugin pour extraire le CSS dans des fichiers séparés
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+  ],
+
+  // Configuration du serveur de développement
   devServer: {
-    contentBase: "./dist",
+    static: './dist',
+    open: true,
+    hot: true,
   },
 };
-
-{
-    test: /\.scss$/,
-    use: [
-      'style-loader', // Injecte le CSS dans le DOM
-      'css-loader',   // Traduit le CSS en CommonJS
-      'sass-loader'   // Compile le SCSS en CSS
-    ],
-  },
-  
